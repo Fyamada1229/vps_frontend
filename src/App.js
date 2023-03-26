657aec20c45ea8dd0de0efa7abd474fdc922fb3b
Fyamada1229 <yamadafutosi1229@gmail.com>
2023-03-26 17:24:58 +0900

    新規画面作成

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import "./App.css";
+import React from "react";
+import "./Login.css";
+import styles from "./styles.module.css";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
-      <h1 className="text-3xl font-bold underline">Hello Tailwind CSS!</h1>
-      <p className="m-0 text-gray-400">Tailwind CSSです</p>
-      <button className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gray-400 hover:text-white">
-        ボタン
-      </button>
-    </div>
+    <>
+      <h1 className="lg:w-1/5 lg:m-auto pt-10 pb-10 smax:w-10/12 smax:m-auto smax:pt-5 ">
+        新規登録
+      </h1>
+      <div className="border w-2/4 m-auto smax:mt-10 smax:w-11/12">
+        <Form className="w-2/5 m-auto smax:w-80">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label className="pt-3">メールアドレス</Form.Label>
+            <Form.Control name="" type="email" placeholder="email" />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label className="pt-3">パスワード</Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Button className={styles.button} type="submit">
+            新規登録
+          </Button>
+          <Button className="ml-10 bg-slate-500">ログイン</Button>
+        </Form>
+      </div>
+    </>
   );
 }
 
